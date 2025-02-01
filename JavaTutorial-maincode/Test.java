a574362e109aeb7ca74d1db9042bd79575821a71
velmurugan-ayyanar <velmuruganayyanar1998@gmail.com>
2025-02-01 21:24:36 +0530

    Remove Complie Class

--- JavaTutorial-maincode/Test.java
+++ JavaTutorial-maincode/Test.java
@@ -1,37 +1,34 @@
-import java.util.Scanner; // Import the Scanner class
 
+
+
+
+import java.util.Scanner;
+
+class ValueTooLowException extends Exception {
+    public ValueTooLowException(String message) {
+        super(message);
+    }
+
+}
 public class Test {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in); // Create a Scanner object
-
-
-      
-
-        if (1<50) {
-
-            System.out.println("assess" );
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            System.out.print("Enter an integer: ");
+            int num = scanner.nextInt();
             
+            if (num < 50) {
+                throw new ValueTooLowException("The entered number is less than 50!");
+            }
+            
+            System.out.println("Valid input: " + num);
+        } catch (ValueTooLowException e) {
+            System.out.println("Exception: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Invalid input! Please enter an integer.");
+        } finally {
+            scanner.close();
         }
-
-           // Read a string input
-
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        
-        System.out.println("Your age");
-        int age = scanner.nextInt();
-
-        // Display the user inputs
-        
-        System.out.println("Name: " + name);
-
-        System.out.println("Age: " + age);
-
-   
-  
-        
-
-        scanner.close(); // Close the scanner
     }
 }
